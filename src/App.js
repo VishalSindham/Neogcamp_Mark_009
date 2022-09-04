420befa23b9f67f1464214f39c3c57752831a37b
Vishal Sindham <37668661+VishalSindham@users.noreply.github.com>
2022-09-04 15:59:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 const moviesDB = {
   actionMovies: [
     { name: "Top Gun: Maverick ", rating: "8.5/10" },
-    { name: "Wild Dog", rating: "6.3/10" }
+    { name: "Wild Dog", rating: "6.3/10" },
+    { name: "The Batman", rating: "7.2/10" }
   ],
   dramas: [
     { name: "Black Bird", rating: "8.2/10" },
-    { name: "Look Both Ways ", rating: "6.3/10" }
+    { name: "Look Both Ways ", rating: "6.3/10" },
+    { name: "Elvis ", rating: "7.6/10" }
   ],
   actionAndAdventure: [
     { name: "Bullet Train", rating: "7.5/10" },
-    { name: "Day Shift", rating: "6.1/10" }
+    { name: "Day Shift", rating: "6.1/10" },
+    { name: "Prey ", rating: "7.2/10" }
   ]
 };
 export default function App() {
